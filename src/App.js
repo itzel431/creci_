c5d2e43a36b24ef003251700c337c8849e177006
itzel431 <ie5123353@gmail.com>
2024-12-15 18:44:00 -0600

    Login funcional y apartado con imagenes en fondo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';  // Para los estilos del encabezado y los apartados
 import Login from './Login';  // Importamos el componente Login
+import logo from './images/logocreci.jpg';  // Importar el logo desde src/images
+import background from './images/background.jpg';  // Importar la imagen de fondo
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showIntro, setShowIntro] = useState(true);  // Estado para mostrar la pantalla inicial
 
   const handleLogin = (username, password) => {
     if (username === 'admin' && password === '1234') {
@@ -12,6 +15,19 @@ function App() {
       alert("Credenciales incorrectas");
     }
   };
+
+  const handleStart = () => {
+    setShowIntro(false);  // Ocultar la pantalla inicial y mostrar el login
+  };
+
+  if (showIntro) {
+    return (
+      <div className="intro-screen">
+        <img src={logo} alt="Logo de Creci" className="logo" />
+        <button onClick={handleStart} className="start-button">Empezamos</button>
+      </div>
+    );
+  }
 
   if (!isLoggedIn) {
     return <Login onLogin={handleLogin} />;
